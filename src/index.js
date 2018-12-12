4e882094dbc6e9743821b3714b19b40d02952655
yapjinai <jinai.jyap@gmail.com>
2018-12-12 16:48:22 -0500

    got switch figured out

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import './css/index.css';
+import App from './containers/App';
+import Home from './containers/Home';
+import About from './containers/About';
+import ErrorPage from './containers/ErrorPage';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <Router>
+    <>
+    <Route path="/" component={App} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </>
+  </Router>),
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
