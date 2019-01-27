c81c49cd1be35d33e0efcda4cc387a745f86fca1
yapjinai <jinai.jyap@gmail.com>
2019-01-27 13:31:08 -0500

    modify browser router to deploy static react-router site to gh-pages

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-import { BrowserRouter as Router } from ‘react-router-dom’;
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './css/index.css';
 
 import App from './components/containers/App';
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById(‘root’));
-// 
+//
 // ReactDOM.render(
 //   <App />,
 //   document.getElementById('root')
