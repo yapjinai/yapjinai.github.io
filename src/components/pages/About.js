da71eccbde28b3be8bd4ab41c0b82345845b6640
yapjinai <jinai.jyap@gmail.com>
2019-08-14 00:16:40 -0400

    feat(about): update bio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import mail from '../../img/about/mail.png';
 import github from '../../img/about/github.png';
 import linkedin from '../../img/about/linkedin.png';
@@ -23,40 +22,27 @@ class About extends Component {
   render() {
     return (
       <>
-
         <div className="page About">
-
           <h1>Jin Ai Yap</h1>
-
           <p>
-            My name is Jin Ai. I am a full stack web developer with experience in Ruby on Rails, React.js, and React-Redux, and I graduated from Yale with a degree in Art.
+            My name is Jin Ai. I am a software engineer working in React.js, Redux-Saga, Django, and Ruby on Rails. I graduated from Yale with a degree in Art.
           </p>
-
           <p>
             When I build apps, I like to imagine how other cultures would use the internet if they had invented it.
           </p>
-
           <h2>Technical skills:</h2>
-
           <p>
-            JavaScript, Ruby on Rails, Python, React.js, React-Redux, Git, HTML5, CSS, Sass, SQL, Adobe Creative Cloud
+            React.js, Redux-Saga, Django, Ruby on Rails, Git, SQL, HTML5, CSS, Sass, Adobe Creative Cloud
           </p>
-
           <h2>I also like:</h2>
-
           <p>
             Climbing, computer music, and cats.
           </p>
-
           <h2>Find me:</h2>
-
           <p><a href='mailto:jinai.jyap@gmail.com' title='email'>Email</a> (jinai.jyap@gmail.com)
           </p>
-
           <p><a href='http://learn.co/yapjinai/resume' title='résumé'>Résumé</a></p>
-
           <IconContainer />
-
           <h6>
             Icons by <a href="https://www.flaticon.com/authors/swifticons" title="Swifticons">Swifticons</a>.
           </h6>
@@ -65,6 +51,4 @@ class About extends Component {
     );
   }
 }
-
 export default About;
-// <a href='mailto:jinai.jyap@gmail.com'><img src={mail} alt='mail' className='icon' /></a>
