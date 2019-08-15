5b3b09675882fe032a3092d9fdfb1fd4078bf34d
yapjinai <jinai.jyap@gmail.com>
2019-08-14 23:35:49 -0400

    style(index.js): remove comments

--- src/index.js
+++ src/index.js
@@ -9,13 +9,5 @@ import './css/index.css';
 import App from './components/containers/App';
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
-//
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
